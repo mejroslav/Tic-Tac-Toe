263f67f2d9ca43c3f5bf12aad483a08b03d83821
mejroslav <miroslav.burysek@gmail.com>
2023-01-31 09:54:56 +0100

    docs: function's description added

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
-let circleTurn;
+let circleTurn; // false if x's on the move, true if o's on the move
 
 const cellElements = document.querySelectorAll("[data-cell]");
-const board = document.getElementById("board");
-
+const boardElement = document.getElementById("board");
 const introMessageScreen = document.querySelector("#introMessage");
+const winningMessageScreen = document.querySelector("#winningMessage");
 const startButton = document.getElementById("start-btn");
-
 const winningMessageTextElement = document.querySelector(
   "[data-winning-message-text]"
 );
-const winningMessageScreen = document.querySelector("#winningMessage");
 const restartButton = document.getElementById("restart-btn");
 
 const WINNING_COMBINATIONS = [
@@ -29,7 +27,9 @@ const WINNING_COMBINATIONS = [
 ];
 
 main();
-
+/** 
+ * The main function. Adds event listener on start button. 
+ */
 function main() {
   startButton.addEventListener("click", (e) => {
     introMessageScreen.classList.add("hide");
@@ -37,6 +37,9 @@ function main() {
   });
 }
 
+/**
+ * Starts the game. Hides winning screen if needed, clears the board and adds listeners to each cell.
+ */
 function startGame() {
   circleTurn = false;
   winningMessageScreen.classList.add("hide");
@@ -50,9 +53,13 @@ function startGame() {
   restartButton.addEventListener("click", startGame);
 }
 
+/**
+ * If the player clicks on a cell, checks whose turn it is and places the mark. If there is a winning combination, shows the winning message screen.
+ */
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+
   // placeMark
   placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
@@ -62,30 +69,45 @@ function handleClick(e) {
     console.log("Remíza!");
     endGame({ draw: true });
   }
+
   // switch turns
   swapTurns();
   setBoardHoverClass();
 }
 
+/**
+ * Places the mark on given cell of given class.
+ */
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
 }
 
+/**
+ * Swaps the turns between x's and o's.
+ */
 function swapTurns() {
   circleTurn = !circleTurn;
 }
 
+/**
+ * Adds the 'x' or 'circle' class on #board depending on whose turn it is.
+ */
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS);
-  board.classList.remove(CIRCLE_CLASS);
+  boardElement.classList.remove(X_CLASS);
+  boardElement.classList.remove(CIRCLE_CLASS);
 
   if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
+    boardElement.classList.add(CIRCLE_CLASS);
   } else {
-    board.classList.add(X_CLASS);
+    boardElement.classList.add(X_CLASS);
   }
 }
 
+/**
+ * Checks if the current player just got a winning combination.
+ * @param {*} currentClass 
+ * @returns true if there is a winning combination
+ */
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some((combination) => {
     return combination.every((index) => {
@@ -94,6 +116,10 @@ function checkWin(currentClass) {
   });
 }
 
+/**
+ * Checks if all cells are fulfilled.
+ * @returns true if all nine squares are fulfilled with 'x' or 'circle' class.
+ */
 function isDraw() {
   return [...cellElements].every((cell) => {
     return (
@@ -102,6 +128,9 @@ function isDraw() {
   });
 }
 
+/**
+ * Shows the winning message screen.
+ */
 function endGame({ draw }) {
   if (draw) {
     winningMessageTextElement.innerText = "Remíza!";
